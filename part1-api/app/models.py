75dbb8f92e262f84e4cac8beac6196b58d963b6e
Hatchet4 <68920180+Hatchet4@users.noreply.github.com>
2025-08-14 17:54:42 +0300

    fix

--- part1-api/app/models.py
+++ part1-api/app/models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, String, DateTime, text
+from sqlalchemy import Column, String, DateTime, text,func
 from sqlalchemy.dialects.postgresql import UUID
 from .database import Base
 
@@ -10,7 +10,14 @@ class Clan(Base):
     name = Column(String, nullable=False)
     region = Column(String, nullable=True)
     created_at = Column(
+    DateTime(timezone=True),
+    server_default=func.now(),  # works in both Postgres and SQLite
+    nullable=False,
+    )
+    '''
+    created_at = Column(
         DateTime(timezone=True),
         server_default=text("(now() at time zone 'utc')"),
         nullable=False,
     )
+    '''

2ae36d0e84ef5fcf10ef7fe0dbaccebab94c98bf
Hatchet4 <68920180+Hatchet4@users.noreply.github.com>
2025-08-14 18:02:47 +0300

    test

--- part1-api/app/models.py
+++ part1-api/app/models.py
@@ -9,12 +9,13 @@ class Clan(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     region = Column(String, nullable=True)
+    '''
     created_at = Column(
     DateTime(timezone=True),
     server_default=func.now(),  # works in both Postgres and SQLite
     nullable=False,
     )
-    '''
+    
     created_at = Column(
         DateTime(timezone=True),
         server_default=text("(now() at time zone 'utc')"),
